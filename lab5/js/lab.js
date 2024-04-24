2d4ab8d423712226ece16e0ad20c9abd43ef5339
bldmochi <natalie.p583@gmail.com>
2024-04-24 15:58:44 -0700

    added lab 5!

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 Data Types and Variable
+// Author: Natalie Peterson
+// Date: 24 April 2024
 
-// Constants
+// Define Variables
+make = " Honda";
+model = " Civic";
+color = " Silver";
+year =  2007;
 
-// Functions
+// calculate
+age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//output
+document.writeln("make: " + make + "<br>");
+document.writeln("model:" + model + "<br>");
+document.writeln("color:" + color + "<br>");
+document.writeln("year:" + year + "<br>");
+document.writeln("age:" + age + "years<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
