927f5100e0597fa7b1ff6c070aee21cfbc362e4b
bldmochi <natalie.p583@gmail.com>
2024-05-12 18:45:43 -0700

    did lab 9!!!!

---
+++ lab9/js/lab.js
@@ -0,0 +1,19 @@
+ /*
+   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: natalie peterson
+   Date: 2024
+*/
+
+// add button in challenge section
+$(".minor-section").append("<button id='button-challenge'> make special </button>");
+
+// add a click listener to the button
+$("#button-challenge").click(function(){
+    
+    //add link to the minor section class
+   $(".minor-section").toggleClass("special");
+
+});
